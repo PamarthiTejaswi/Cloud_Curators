88272f30e7a976d916db016d8074777c56cf3824
Sanaka Vineela <168650739+Vineela-16@users.noreply.github.com>
2026-03-24 15:00:25 +0530

    Add files via upload

---
+++ DTOs/Hotel/CreateHotelDto.cs
@@ -0,0 +1,10 @@
+﻿namespace HotelBookingWebsite.DTOs.Hotel
+{
+	public class CreateHotelDto
+	{
+		public string Name { get; set; } = string.Empty;
+		public string Location { get; set; } = string.Empty;
+		public string Description { get; set; } = string.Empty;
+		public string Amenities { get; set; } = string.Empty;
+	}
+}
---
+++ DTOs/Hotel/HotelResponseDto.cs
@@ -0,0 +1,10 @@
+﻿namespace HotelBookingWebsite.DTOs.Hotel
+{
+	public class HotelResponseDto
+	{
+		public int Id { get; set; }
+		public string Name { get; set; } = string.Empty;
+		public string Location { get; set; } = string.Empty;
+		public string Amenities { get; set; } = string.Empty;
+	}
+}
